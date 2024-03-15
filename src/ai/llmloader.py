5f106915fa7860d0a379216f545d5ac44b72c5f6
nusaturn <skaturanus@proton.me>
2024-03-15 19:43:49 +0530

    modified llm loading a lot

--- src/ai/llmloader.py
+++ src/ai/llmloader.py
@@ -2,7 +2,7 @@ import getpass
 import os
 from langchain_community.llms.llamacpp import LlamaCpp
 from langchain_community.llms.fireworks import Fireworks
-from pydantic_core.core_schema import no_info_after_validator_function
+from langchain_community.llms.ollama import Ollama
 
 config = {}
 
@@ -11,27 +11,17 @@ def set_opts(cfg: dict):
     global config
     config = cfg.copy()
 
-    global temp, ctx_len, max_tokens, n_gpu_layers
-    temp = float(config["model_temp"]) if config["model_temp"] is not None else 0.75
-    ctx_len = (
-        int(config["model_ctx_len"]) if config["model_ctx_len"] is not None else 4096
-    )
-    max_tokens = (
-        int(config["model_max_new_tokens"])
-        if config["model_max_new_tokens"] is not None
-        else 2048
-    )
-    n_gpu_layers = (
-        int(config["model_gpu_layers"])
-        if config["model_gpu_layers"] is not None
-        else 24
-    )
-    return temp, ctx_len, max_tokens, n_gpu_layers
-
 
 def load_local_llm():
+    cfg = config["local"]
+    temp = float(cfg["temp"]) if cfg["temp"] is not None else 0.75
+    ctx_len = int(cfg["ctx_len"]) if cfg["ctx_len"] is not None else 2048
+    max_tokens = (
+        int(cfg["max_new_tokens"]) if cfg["max_new_tokens"] is not None else 1024
+    )
+    n_gpu_layers = int(cfg["gpu_layers"]) if cfg["gpu_layers"] is not None else 24
     llm = LlamaCpp(
-        model_path=config["model_path"],
+        model_path=cfg["model_path"],
         temperature=temp,
         n_gpu_layers=n_gpu_layers,
         n_ctx=ctx_len,
@@ -43,21 +33,57 @@ def load_local_llm():
 def load_fireworks_llm():
     if "FIREWORKS_API_KEY" not in os.environ:
         os.environ["FIREWORKS_API_KEY"] = getpass.getpass("Fireworks API Key:")
+    cfg = config["fireworks"]
+
+    temp = float(cfg["model_temp"]) if cfg["model_temp"] is not None else 0.75
+    max_tokens = (
+        int(cfg["model_max_tokens"]) if cfg["model_max_tokens"] is not None else 1024
+    )
 
     # Initialize a Fireworks model
     llm = Fireworks(
         model="accounts/fireworks/models/mistral-7b-instruct-v0p2",
         base_url="https://api.fireworks.ai/inference/v1/completions",
-        max_tokens=ctx_len,
+        max_tokens=max_tokens,
         temperature=temp,
     )
     return llm
 
 
-def load_llm():
-    if config["llm"] == "local":
+def load_ollama_llm():
+    cfg = config["ollama"]
+    temp = float(cfg["temp"]) if cfg["temp"] is not None else 0.75
+    ctx_len = int(cfg["ctx_len"]) if cfg["ctx_len"] is not None else 2048
+    max_tokens = (
+        int(cfg["max_new_tokens"]) if cfg["max_new_tokens"] is not None else 1024
+    )
+    llm = Ollama(
+        model=cfg["model"],
+        num_ctx=ctx_len,
+        num_predict=max_tokens,
+        temperature=temp,
+        num_gpu=cfg["num_gpu"],
+        format=cfg["format"],
+        headers=cfg["headers"],
+        mirostat=cfg["mirostat"],
+        cache=True,
+    )
+    return llm
+
+
+def load_llm(provider="local"):
+    if provider == "local":
         return load_local_llm()
-    elif config["llm"] == "fireworks":
-        return load_fireworks_llm()
-    else:
+    elif provider == "fireworks":
         return load_local_llm()
+    elif provider == "ollama":
+        return load_local_llm()
+
+
+def load_all_llms():
+    llms = config["llms"]
+    llms = llms if llms is list else [llms]
+    loaded = dict.fromkeys(llms)
+    for provider in loaded:
+        loaded[provider] = load_llm(provider)
+    return loaded
